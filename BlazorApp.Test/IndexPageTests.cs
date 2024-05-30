f0d591cd9954e8c2212cca536c786340d9254d53
José Federico Bertoncini <fede.berton@gmail.com>
2024-05-30 18:13:41 -0300

    Agregado de test

---
+++ BlazorApp.Test/IndexPageTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BlazorApp.Pages;
+using Bunit;
+
+namespace BlazorApp.Test
+{
+    public class IndexPageTests: TestContext
+    {
+        [Fact]
+        public void IndexPage_HasCorrectTitle()
+        {
+            // Renderizar el componente Index
+            var cut = RenderComponent<BlazorApp.Pages.Index>();
+
+            // Comprobar si el título contiene "Hola mundo loco"
+            cut.MarkupMatches("<h1>Hola mundo loco</h1>");
+        }
+    }
+}
---
+++ BlazorApp.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace BlazorApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = true; // Simulación de una prueba que pasa
+            Assert.True(result);
+        }
+    }
+}
